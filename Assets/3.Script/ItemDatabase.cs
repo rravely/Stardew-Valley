73f140ea0e8d61065f9da6becf8a96a875a8691f
rahyun <rravely@naver.com>
2023-04-13 10:31:19 +0900

    Complete load player data

--- Assets/3.Script/ItemDatabase.cs
+++ Assets/3.Script/ItemDatabase.cs
@@ -2,16 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemDatabase : MonoBehaviour
+public static class ItemDatabase
 {
-    public List<Item> items = new List<Item>();
+    public static List<Item> items = new List<Item>() {
+            new Item(0, "Hoe", "Used to dig up dirt."),
+            new Item(1, "Pickaxe", "Used to smash rocks and mine."),
+            new Item(2, "Axe", "Used to chop wood."),
+            new Item(3, "Wateringcan", "Used to water your crops."),
+            new Item(4, "Scythe", "Used to cut down grass and plants.")
 
-    private void Awake() 
-    {
-        BuildDatabase();
-    }
 
-    public Item GetItem(int Id) {
+
+            };
+
+
+    public static Item GetItem(int Id) {
         for (int i = 0; i < items.Count; i++) {
             if (items[i].Id.Equals(Id)) {
                 return items[i];
@@ -21,20 +26,8 @@ public class ItemDatabase : MonoBehaviour
         //return items.Find(item => item.Id == Id);
     }
 
-    public Item GetItem(string ItemName) {
+    public static Item GetItem(string ItemName) {
         return items.Find(item => item.ItemName == ItemName);
     }
 
-    void BuildDatabase() {
-        items = new List<Item>() {
-            new Item(0, "Hoe", "Used to dig up dirt."),
-            new Item(1, "Pickaxe", "Used to smash rocks and mine."),
-            new Item(2, "Axe", "Used to chop wood."),
-            new Item(3, "Wateringcan", "Used to water your crops."),
-            new Item(4, "Scythe", "Used to cut down grass and plants.")
-            
-            
-            };
-
-    }
 }
--- Assets/3.Script/Player/PlayerInventory.cs
+++ Assets/3.Script/Player/PlayerInventory.cs
@@ -9,31 +9,38 @@ public class PlayerInventory : MonoBehaviour
     [SerializeField]private GameManager gameManager;
 
     public SaveData player;
-    public ItemDatabase itemDatabase;
     public UIInventory inventoryUI;
     public bool activeInventory2Line = false, activeInventory3Line = false;
 
     void Start() {
         //현재 플레이어의 savedata를 가지고 와서 저장
         player = gameManager.player;
+        Debug.Log(player.saveInventory.Count);
+        for (int i = 0; i < player.saveInventory.Count; i++)
+        {
+            inventoryUI.AddNewItem(player.saveInventory[i]);
+        }
+    }
 
-        Debug.Log(player.name);
+    void Update() {
+        
+        
     }
 
 
     public void GiveItem(int Id) 
     {
-        Item itemToAdd = itemDatabase.GetItem(Id);
+        Item itemToAdd = ItemDatabase.GetItem(Id);
         inventory.Add(itemToAdd);
         //Debug.Log(itemToAdd.ItemName);
-        inventoryUI.AddNewItem(itemToAdd); 
+        //inventoryUI.AddNewItem(itemToAdd); 
     }
 
     public void GiveItem(string ItemName)
     {
-        Item itemToAdd = itemDatabase.GetItem(ItemName);
+        Item itemToAdd = ItemDatabase.GetItem(ItemName);
         inventory.Add(itemToAdd);
-        inventoryUI.AddNewItem(itemToAdd);
+        //inventoryUI.AddNewItem(itemToAdd);
     }
 
     public Item checkForItem(int Id) 
@@ -47,7 +54,7 @@ public class PlayerInventory : MonoBehaviour
         if (itemToRemove != null)
         {
             inventory.Remove(itemToRemove);
-            inventoryUI.RemoveItem(itemToRemove);
+            //inventoryUI.RemoveItem(itemToRemove);
         }
     }
 }
