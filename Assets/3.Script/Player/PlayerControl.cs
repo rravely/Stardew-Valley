b1eb6b4efbb579207bbfaee6a78756bd2b867b63
rahyun <rravely@naver.com>
2023-04-16 00:04:23 +0900

    Mouse click and get item

--- Assets/3.Script/Player/PlayerControl.cs
+++ Assets/3.Script/Player/PlayerControl.cs
@@ -8,10 +8,13 @@ public class PlayerControl : MonoBehaviour
     private Animator animator;
     private string animationState = "player_walk";
     [HideInInspector]public int playerDirection = 0; //플레이어가 움직이는 방향
+    [HideInInspector]public int workDirection = 0; //플레이어 일하는 방향
     [HideInInspector] public int selectedToolId = -1; //플레이어가 선택한 도구 (-1은 아직 선택 x)
     private SpriteRenderer spriteRenderer; 
     private float playerPosZ; //플레이어와 건물 사이의 z 위치 비교를 위해
     [SerializeField] private Sprite[] playerSprite = new Sprite[4]; //방향에 따라 가만히 있는 sprite
+    private float deltaX, deltaY, slope;
+    public Camera camera;
     
 
     enum PLAYERWALKSTATE{
@@ -46,6 +49,7 @@ public class PlayerControl : MonoBehaviour
     {
         ChangeDirection();
         ChangeZSameAsY();
+        MouseClickForWork();
     }
 
     void ChangeDirection() {
@@ -119,6 +123,40 @@ public class PlayerControl : MonoBehaviour
                 break;
             case 4: //낫
                 break;
+        }
+    }
+
+    void MouseClickForWork()
+    {
+        if (Input.GetMouseButtonDown(0) && selectedToolId != -1) //마우스가 눌리고 선택된 아이템이 도구라면
+        {
+            //방향 구하기
+            deltaX = camera.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x;
+            deltaY = camera.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y;
+            slope = deltaY / deltaX;
+
+            if ((deltaX > 0 && slope > 0 && slope < 1) || (deltaX > 0 && slope < 0 && slope > -1)) //right
+            {
+                //그 자리에 서서 플레이어 오른쪽 애니메이션
+                //오른쪽 방향의 오브젝트와 충돌중이면 그 오브젝트 파괴하고 아이템 얻기
+                workDirection = 1;
+                Debug.Log("right");
+            }
+            else if ((deltaX < 0 && slope > 0 && slope < 1) || (deltaX < 0 && slope < 0 && slope > -1)) //left
+            {
+                workDirection = 2;
+                Debug.Log("left");
+            }
+            else if (deltaY > 0) //up
+            {
+                workDirection = 3;
+                Debug.Log("Up");
+            }
+            else //down
+            {
+                workDirection = 4;
+                Debug.Log("Down");
+            }
         }
     }
 
