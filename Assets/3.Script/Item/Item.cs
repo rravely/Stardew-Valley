b5158043059f6335a8770c5f629c3d9557110eea
rahyun <rravely@naver.com>
2023-04-18 11:38:29 +0900

    Items view of shop

--- Assets/3.Script/Item/Item.cs
+++ Assets/3.Script/Item/Item.cs
@@ -8,6 +8,7 @@ public class Item : ScriptableObject
     public int id;
     public string itemName;
     public string itemDescription;
+    public int cost;
     public Sprite icon;
     public bool stackable = true;
     public bool isTool = false;
---
+++ Assets/3.Script/Item/ShopItem.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopItem : MonoBehaviour
+{
+    public Item item;
+    private Image itemIcon;
+    private Text itemName;
+    private Text itemCost;
+
+    void Start() {
+        itemIcon = transform.GetChild(0).GetComponent<Image>();
+        itemName = transform.GetChild(1).GetComponent<Text>();
+        itemCost = transform.GetChild(2).GetComponent<Text>();
+
+        itemIcon.sprite = item.icon;
+        itemName.text = item.itemName;
+        itemCost.text = item.cost.ToString();
+    }
+
+    void Update() {
+        
+    }
+}
